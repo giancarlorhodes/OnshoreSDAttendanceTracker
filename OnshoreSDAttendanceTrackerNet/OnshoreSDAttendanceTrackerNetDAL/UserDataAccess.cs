2703fd15d8ae4a4315e9c1f1a19820c8b7393bc0
brittani.johnson <brittani.johnson@onshoreoutsourcing.com>
2019-05-09 16:18:38 -0500

    full CRUD userDAL

--- OnshoreSDAttendanceTrackerNet/OnshoreSDAttendanceTrackerNetDAL/UserDataAccess.cs
+++ OnshoreSDAttendanceTrackerNet/OnshoreSDAttendanceTrackerNetDAL/UserDataAccess.cs
@@ -8,61 +8,120 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
 using OnshoreSDAttendanceTrackerNetDAL.Models;
+using OnshoreSDAttendanceTrackerErrorLogger;
 
 namespace OnshoreSDAttendanceTrackerNetDAL
 {
     public class UserDataAccess
     {
-        private string _ConnectionString;
+        private string _ConnectionString = (ConfigurationManager.ConnectionStrings["OnshoreSDAttendanceTracker"].ConnectionString);
 
-        public UserDataAccess(string iConnectionString)
-        {
-            _ConnectionString = iConnectionString;
-        }
 
+
+        #region CreateUser
         public void CreateUser(IUserDO iUser)
         {
             try
             {
-                using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["OnshoreSDAttendanceTracker"].ConnectionString))
+                using (SqlConnection conn = new SqlConnection(_ConnectionString))
                 {
-                    using (SqlCommand comm = new SqlCommand("sp_MakeUser", conn))
+                    using (SqlCommand createComm = new SqlCommand("sp_MakeUser", conn))
                     {
                         try
                         {
-                            comm.CommandType = CommandType.StoredProcedure;
-                            comm.CommandTimeout = 35;
+                            createComm.CommandType = CommandType.StoredProcedure;
+                            createComm.CommandTimeout = 35;
 
-                            comm.Parameters.AddWithValue("@CreatedByUserId", SqlDbType.Int).Value = iUser.UserID;
-                            comm.Parameters.AddWithValue("@TeamId", SqlDbType.Int).Value = iUser.TeamID;
-                            comm.Parameters.AddWithValue("@RoldeId", SqlDbType.Int).Value = iUser.RoleID_FK;
-                            comm.Parameters.AddWithValue("@Email", SqlDbType.VarChar).Value = iUser.Email;
-                            comm.Parameters.AddWithValue("@FName", SqlDbType.VarChar).Value = iUser.FirstName;
-                            comm.Parameters.AddWithValue("@LName", SqlDbType.VarChar).Value = iUser.LastName;
+                            createComm.Parameters.AddWithValue("@CreatedByUserId", SqlDbType.Int).Value = iUser.UserID;
+                            createComm.Parameters.AddWithValue("@TeamId", SqlDbType.Int).Value = iUser.TeamID;
+                            createComm.Parameters.AddWithValue("@RoldeId", SqlDbType.Int).Value = iUser.RoleID_FK;
+                            createComm.Parameters.AddWithValue("@Email", SqlDbType.VarChar).Value = iUser.Email;
+                            createComm.Parameters.AddWithValue("@FName", SqlDbType.VarChar).Value = iUser.FirstName;
+                            createComm.Parameters.AddWithValue("@LName", SqlDbType.VarChar).Value = iUser.LastName;
 
                         }
                         catch (Exception ex)
                         {
-                            throw;
+                            ErrorLogger.LogError(ex, "CreateUser", "nothing");
                         }
+
                         finally
                         {
                             conn.Close();
                             conn.Dispose();
-                            conn.Dispose();
+                            createComm.Dispose();
                         }
                     }
                 }
             }
             catch (Exception ex)
             {
-                throw;
+                ErrorLogger.LogError(ex, "CreateUser", "nothing");
             }
+
             finally
             {
 
             }
         }
+
+        #endregion
+
+        #region Gets
+
+        public IUserDO GetUserByID(int iUserID)
+        {
+            IUserDO user = new UserDO();
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(_ConnectionString))
+                {
+                    using (SqlCommand getUserComm = new SqlCommand("sp_GetUserById", con))
+                    {
+                        try
+                        {
+                            getUserComm.CommandType = CommandType.StoredProcedure;
+                            getUserComm.CommandTimeout = 35;
+
+                            getUserComm.Parameters.AddWithValue("@UserId", iUserID);
+                            con.Open();
+
+                            using (SqlDataReader reader = getUserComm.ExecuteReader())
+                            {
+                                user.UserID = reader.GetInt32(reader.GetOrdinal("UserID"));
+                                user.FirstName = (string)reader["FirstName"];
+                                user.LastName = (string)reader["LastName"];
+                                user.RoleID_FK = reader.GetInt32(reader.GetOrdinal("RoleID"));
+                                user.Email = (string)reader["Email"];
+                                user.TeamID = reader.GetInt32(reader.GetOrdinal("TeamID"));
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            ErrorLogger.LogError(ex, "GetUserByID", "nothing");
+                        }
+                        finally
+                        {
+                            con.Close();
+                            con.Dispose();
+                            getUserComm.Dispose();
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.LogError(ex, "GetUserByID", "nothing");
+            }
+
+            return user;
+
+        }
+
+
+
 
 
         public List<IUserDO> GetAllUsers()
@@ -71,7 +130,7 @@ namespace OnshoreSDAttendanceTrackerNetDAL
 
             try
             {
-                using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["OnshoreSDAttendanceTracker"].ConnectionString))
+                using (SqlConnection conn = new SqlConnection(_ConnectionString))
                 {
 
                     using (SqlCommand getComm = new SqlCommand("sp_GetUsers"))
@@ -83,7 +142,7 @@ namespace OnshoreSDAttendanceTrackerNetDAL
                         using (SqlDataReader reader = getComm.ExecuteReader())
                         {
                             while (reader.Read())
-                            {//UserID,FirstName,LastName,RoleID_FK as RoleID,Email
+                            {
                                 IUserDO user = new UserDO();
                                 user.UserID = reader.GetInt32(reader.GetOrdinal("UserID"));
                                 user.FirstName = (string)reader["FirstName"];
@@ -101,13 +160,90 @@ namespace OnshoreSDAttendanceTrackerNetDAL
             }
             catch (Exception ex)
             {
-                throw;
+                ErrorLogger.LogError(ex, "GetAllUsers", "nothing");
             }
 
             return listOfDBUsers;
         }
 
+        #endregion
 
+        #region Updates
+        //updates User info and takes in OldTeamID to update in SP where UserID & OldTeamID equal in TeamManagement table
+        public void UpdateUser(IUserDO iUser, int OldTeamID)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(_ConnectionString))
+                {
+                    using (SqlCommand updateComm = new SqlCommand("sp_UpdateUser", conn))
+                    {
+                        try
+                        {
+                            updateComm.Parameters.AddWithValue("@UserID", iUser.UserID);
+                            updateComm.Parameters.AddWithValue("@ModifiedByUserId", iUser.UserID);
+                            updateComm.Parameters.AddWithValue("@RoleId", iUser.RoleID_FK);
+                            updateComm.Parameters.AddWithValue("@OldTeamId", OldTeamID);
+                            updateComm.Parameters.AddWithValue("@NewTeamId", iUser.TeamID);
+                            updateComm.Parameters.AddWithValue("@Email", iUser.Email);
+                            updateComm.Parameters.AddWithValue("@FName", iUser.FirstName);
+                            updateComm.Parameters.AddWithValue("@LName", iUser.LastName);
+
+                        }
+                        catch (Exception ex)
+                        {
+                            ErrorLogger.LogError(ex, "UpdateUser", "nothing");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.LogError(ex, "UpdateUser", "nothing");
+            }
+        }
+
+        #endregion
+
+        #region Deletes
+        public void RemoveUser(int userToDelID, int modifiedByUserID)
+        {
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(_ConnectionString))
+                {
+                    using (SqlCommand deleteComm = new SqlCommand("sp_RemoveUserById", conn))
+                    {
+                        try
+                        {
+                            deleteComm.CommandType = CommandType.StoredProcedure;
+                            deleteComm.CommandTimeout = 35;
+                            deleteComm.Parameters.AddWithValue("@UserId", userToDelID);
+                            deleteComm.Parameters.AddWithValue("@ModifiedByUserId ", modifiedByUserID);
+
+                            deleteComm.ExecuteNonQuery();
+                        }
+                        catch (Exception ex)
+                        {
+                            ErrorLogger.LogError(ex, "UpdateUser", "nothing");
+                        }
+                        finally
+                        {
+                            conn.Close();
+                            conn.Dispose();
+                            deleteComm.Dispose();
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.LogError(ex, "UpdateUser", "nothing");
+            }
+        }
+        #endregion
 
 
     }

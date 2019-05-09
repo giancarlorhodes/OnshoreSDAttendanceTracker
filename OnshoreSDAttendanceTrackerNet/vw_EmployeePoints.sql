c309fc624800de0215bffd7da8f41cee03912792
grhodes29 <grhodes29@gmail.com>
2019-05-09 10:32:05 -0500

    new views

---
+++ OnshoreSDAttendanceTrackerNet/vw_EmployeePoints.sql
@@ -0,0 +1,24 @@
+﻿USE [OnshoreSDAttendanceTracker]
+GO
+
+/****** Object:  View [dbo].[vw_EmployeePoints]    Script Date: 5/9/2019 10:21:11 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE VIEW [dbo].[vw_EmployeePoints]
+AS
+SELECT	vttmu.TeamName, 
+		vttmu.FirstName, 
+		vttmu.LastName, 
+		abt.[Name] AS AbsenceType, 
+		abt.Point, pb.CreateDate  
+FROM [dbo].[vw_TeamTeamManagementUser] vttmu
+	INNER JOIN [dbo].[PointBank] pb ON pb.TeamManagementID_FK = vttmu.TeamManagementID
+	INNER JOIN [dbo].[AbsenceType]  abt ON abt.AbsenceTypeID = pb.AbsenceTypeID_FK
+
+
+GO
---
+++ OnshoreSDAttendanceTrackerNet/vw_TeamTeamManagementUser.sql
@@ -0,0 +1,32 @@
+﻿USE [OnshoreSDAttendanceTracker]
+GO
+
+/****** Object:  View [dbo].[vw_TeamTeamManagementUser]    Script Date: 5/9/2019 10:23:39 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE VIEW [dbo].[vw_TeamTeamManagementUser]
+AS
+SELECT   
+    tm.TeamManagementID,
+	tm.Active AS TeamManagementActive,    
+	t.TeamID, 
+	t.[Name] As TeamName, 
+	u.UserID, 
+	u.FirstName, 
+	u.LastName,
+	r.RoleID,
+	r.RoleNameShort,
+	r.RoleNameLong
+	
+FROM 
+	Team t INNER JOIN TeamManagement tm ON t.TeamID = tm.TeamID_FK 
+	INNER JOIN [User] u ON tm.UserID_FK = u.UserID
+	INNER JOIN [Role] r on r.RoleID = u.RoleID_FK
+
+GO

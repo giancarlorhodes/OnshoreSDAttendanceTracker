de65ab8b20d1febac96c06b4ccb49cd592d4da2c
= <jeremiah.carpenter@onshoreoutsourcing.com>
2019-05-10 10:14:48 -0500

    Created the files for boilerplate for each of the view models so each member can get experience with View Models using constructors. Also, included the MaintenanceController boilerplate.

---
+++ OnshoreSDAttendanceTrackerNet/OnshoreSDAttendanceTrackerNet/Models/TeamViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnshoreSDAttendanceTrackerNet.Models
+{
+    public class TeamViewModel
+    {
+        // TODO: Implement the Team View Model
+    }
+}
---
+++ OnshoreSDAttendanceTrackerNet/OnshoreSDAttendanceTrackerNet/Models/UserCredentialsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnshoreSDAttendanceTrackerNet.Models
+{
+    public class UserCredentialsViewModel
+    {
+        // TODO: Implement User Credentials View Model
+    }
+}

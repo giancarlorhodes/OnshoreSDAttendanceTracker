6e415f5a97ec6f3b41a2e28fde094f0423ea726d
grhodes29 <grhodes29@gmail.com>
2019-05-22 09:37:15 -0500

    need move my login stuff into here. Oops. Maybe look at Trello before starting my coding session. You think!??!

---
+++ OnshoreSDAttendanceTrackerNet/OnshoreSDAttendanceTrackerNet/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿
+namespace OnshoreSDAttendanceTrackerNet.Controllers
+{
+
+
+
+    using OnshoreSDAttendanceTrackerNet.Common;
+    using OnshoreSDAttendanceTrackerNet.Models;
+    using OnshoreSDAttendanceTrackerNetDAL;
+    using OnshoreSDAttendanceTrackerNetDAL.Interfaces;
+    using OnshoreSDAttendanceTrackerNetDAL.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security.Principal;
+    using System.Threading;
+    using System.Web;
+    using System.Web.Configuration;
+    using System.Web.Mvc;
+    using System.Web.Security;
+
+    /// <summary>
+    /// Company: Onshore Outsourcing. https://www.onshoreoutsourcing.com/
+    /// Author: Giancarlo Rhodes, Date: 5/18/2019
+    /// Description: We could use this controller but mostly we need to use this to test
+    /// how to get role and authetication, authorization logic
+    /// The only anonymous request possible to the site should only be to login method.
+    /// After that the login response should determin identity and role of the user and 
+    /// save it to session.
+    /// </summary>
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

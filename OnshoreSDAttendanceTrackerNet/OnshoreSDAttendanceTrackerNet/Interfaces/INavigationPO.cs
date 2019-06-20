111e34f3d01f8ed5ac6941a9a9dff9cd7df942e5
Aleris Roman <aleris.roman@onshoreoutsourcing.com>
2019-06-19 21:55:34 -0400

    Added Nav Models and Mappers

---
+++ OnshoreSDAttendanceTrackerNet/OnshoreSDAttendanceTrackerNet/Interfaces/INavigationPO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnshoreSDAttendanceTrackerNet.Interfaces
+{
+    public interface INavigationPO
+    {
+        int NavigationID { get; set; }
+        string MenuItem { get; set; }
+        string URL { get; set; }
+        int RoleID { get; set; }
+        int ParentNavigationID { get; set; }
+        int Order { get; set; }
+    }
+}
---
+++ OnshoreSDAttendanceTrackerNet/OnshoreSDAttendanceTrackerNetBLL/Models/NavigationBO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnshoreSDAttendanceTrackerNetBLL.Interfaces;
+
+namespace OnshoreSDAttendanceTrackerNetBLL.Models
+{
+    public class NavigationBO:INavigationBO
+    {
+        public int NavigationID { get; set; }
+        public string MenuItem { get; set; }
+        public string URL { get; set; }
+        public int RoleID { get; set; }
+        public int ParentNavigationID { get; set; }
+        public int Order { get; set; }
+    }
+}

f9a1ecdabb35f8bfd56a917996e074b0956cd0ca
Aleris Roman <aleris.roman@onshoreoutsourcing.com>
2019-06-24 22:14:21 -0400

    Fixed ErrorLogger initialize issue

--- OnshoreSDAttendanceTrackerNet/OnshoreSDAttendanceTrackerErroLogger/ErrorLogger.cs
+++ OnshoreSDAttendanceTrackerNet/OnshoreSDAttendanceTrackerErroLogger/ErrorLogger.cs
@@ -12,17 +12,28 @@ namespace OnshoreSDAttendanceTrackerErrorLogger
     public static class ErrorLogger
     {
         //TODO:  Match DBContext to correct Web.config name for DB
-        private static string conString= ConfigurationManager.ConnectionStrings["DBContext"].ConnectionString;
+        private static string conString;
 
-        public static void LogError(Exception error, string location,string url)
+        static ErrorLogger()
         {
+            conString = ConfigurationManager.ConnectionStrings["OnshoreSDAttendanceTracker"].ConnectionString;
+        }
+
+        public static void LogError(Exception error, string location, string url)
+        {
+            string innerEx = "";
+            if (error.InnerException != null)
+            {
+                innerEx = error.InnerException.Message.ToString();
+            }
             using (SqlConnection con = new SqlConnection(conString))
             {
                 using (SqlCommand com = new SqlCommand("sp_LogError", con))
                 {
+                    con.Open();
                     com.CommandType = CommandType.StoredProcedure;
-                    com.Parameters.Add(new SqlParameter("@ErrorMsg", error.InnerException.Message.ToString()));
-                    com.Parameters.Add(new SqlParameter("@SPName",location));
+                    com.Parameters.Add(new SqlParameter("@ErrorMsg", innerEx));
+                    com.Parameters.Add(new SqlParameter("@SPName", location));
                     com.Parameters.Add(new SqlParameter("@ExceptionType", error.GetType().ToString()));
                     com.Parameters.Add(new SqlParameter("@ExceptionSource", "Application"));
                     com.Parameters.Add(new SqlParameter("@ExceptionURL", url));
